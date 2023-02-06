a76a66d46dca3654e1baad16039b3b1059d116ce
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2023-02-06 22:21:13 +0100

    update

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -1,5 +1,6 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import at.ac.fhcampuswien.fhmdb.models.Genre;
 import at.ac.fhcampuswien.fhmdb.models.Movie;
 import at.ac.fhcampuswien.fhmdb.ui.MovieCell;
 import com.jfoenix.controls.JFXButton;
@@ -12,6 +13,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -45,6 +47,8 @@ public class HomeController implements Initializable {
 
         // TODO add genre filter items with genreComboBox.getItems().addAll(...)
         genreComboBox.setPromptText("Filter by Genre");
+        genreComboBox.getItems().add(0, 0);
+        genreComboBox.getItems().addAll(FXCollections.observableArrayList(Genre.values()));
 
         // TODO add event handlers to buttons and call the regarding methods
         // either set event handlers in the fxml file (onAction) or add them here
@@ -52,14 +56,28 @@ public class HomeController implements Initializable {
         // Sort button example:
         sortBtn.setOnAction(actionEvent -> {
             if(sortBtn.getText().equals("Sort (asc)")) {
+                observableMovies.sort(Comparator.comparing(Movie::getTitle));
                 // TODO sort observableMovies ascending
                 sortBtn.setText("Sort (desc)");
             } else {
                 // TODO sort observableMovies descending
+                observableMovies.sort(Comparator.comparing(Movie::getTitle));
                 sortBtn.setText("Sort (asc)");
             }
         });
 
+        searchBtn.setOnAction(actionEvent -> {
+            String query = searchField.getText().toLowerCase(); // Texteingabe des Benutzers im Suchfeld
+            Genre selectedGenre = (Genre) genreComboBox.getValue();
+
+            List<Movie> filteredMovies = (List<Movie>) allMovies.stream().
+                    filter(movie -> movie.getTitle().toLowerCase().contains(query) || movie.getDescription().toLowerCase().
+                            contains(query)).filter(movie -> selectedGenre == null || movie.getGenres().
+                            contains(selectedGenre)).toList();
+
+            observableMovies.addAll(filteredMovies);
+
+        });
 
     }
 }
